79b5117ae964a0a39e0652f32314cbe4d0747cce
fmatorin <f.matorin@mail.ru>
2025-03-29 12:34:06 +0300

    Добавлен пример работы с файлом

--- src/main/java/ru/mtuci/bvt_demo/MyFileController.java
+++ src/main/java/ru/mtuci/bvt_demo/MyFileController.java
@@ -11,7 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.LinkedList;
 import java.util.Optional;
+import java.util.Queue;
 
 @RestController
 @RequestMapping("/files")
@@ -23,7 +30,7 @@ public class MyFileController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> saveFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> saveFile(@RequestParam("file") MultipartFile file) throws IOException {
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().body("Empty file");
         }
@@ -42,6 +49,47 @@ public class MyFileController {
         } catch (IOException ex) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
         }
+    }
+
+    @PostMapping
+    public void processFile(@RequestParam("file") MultipartFile file) throws IOException {
+        //Сохраняем файл из запроса во временный бинарный файл
+        Path tempfile = Files.createTempFile("upload-", ".bin");
+        try (InputStream is = file.getInputStream()) {
+            Files.copy(is, tempfile, StandardCopyOption.REPLACE_EXISTING);
+        }
+        //Читаем временный файл блоками
+        try (RandomAccessFile raf = new RandomAccessFile(tempfile.toFile(), "r")) {
+            final int windowSize = 8;
+            //Размер блока для чтения в байтах
+            final int chunkSize = 8192;
+            byte[] chunk = new byte[chunkSize];
+            //Текущая позиция в файле
+            long globalPos = 0;
+            int bytesRead;
+            long rollingHash = 0;
+            Queue<Byte> windowQueue = new LinkedList<>();
+
+            //Читаем, пока не дойдём до конца файла
+            while((bytesRead = raf.read(chunk)) != -1) {
+                //Какая-то логика работы с файлом
+                for (int i = 0; i < windowSize; i++) {
+                    byte b = chunk[i];
+                    rollingHash = increase_rollingHash(byte);
+                    windowQueue.add(b);
+
+                    if (windowQueue.size() == windowSize) {
+                        //Выполняем поиск сигнатуры по словарю
+                    }
+                    byte oldestByte = windowQueue.remove();
+                    long oldestVal = remove_byte_from_rolling_hash(oldestByte);
+                    rollingHash = (rollingHash - oldestVal + MOD) % MOD;
+                }
+                globalPos++;
+            }
+        }
+        //...
+        Files.deleteIfExists(tempfile);
     }
 
     @GetMapping("/download/{id}")
